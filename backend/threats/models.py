9f1ea75b31ee21f4f441e9d8b345c842d7795620
mikey6453 <tvq3bn@virginia.edu>
2025-03-25 23:30:50 -0400

    create model for dataset

--- backend/threats/models.py
+++ backend/threats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class CyberAttack(models.Model):
+    country = models.CharField(max_length=100)
+    year = models.IntegerField()
+    attack_type = models.CharField(max_length=100)
+    target_industry = models.CharField(max_length=100)
+    financial_loss_million = models.FloatField()
+    affected_users = models.BigIntegerField()
+    attack_source = models.CharField(max_length=100)
+    vulnerability_type = models.CharField(max_length=100)
+    defense_mechanism = models.CharField(max_length=100)
+    resolution_time_hours = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.country} - {self.year} - {self.attack_type}"
